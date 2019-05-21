b816128b619a2ca0adba1951eec9a7f4c6416261
Niggolard <oonoggaoo@gmail.com>
2019-05-21 16:53:57 +0200

    added item in world

--- Assets/Items/Item.cs
+++ Assets/Items/Item.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
-public class Item
+public static class Item
 {
     public enum Type{
         UNDEF,
@@ -15,6 +15,7 @@ public class Item
     };
 
     private static Texture2D[] sprites;
+    private static Material[] itemMaterial;
 
     public static void loadSprites() {
         sprites = new Texture2D[names.Length];
@@ -23,6 +24,26 @@ public class Item
             if (File.Exists(filePath)) {
                 sprites[i] = new Texture2D(128, 64);
                 sprites[i].LoadImage(File.ReadAllBytes(filePath));
+            }
+        }
+    }
+
+    public static void loadMaterial() {
+        itemMaterial = new Material[names.Length];
+        for (int i = 0;i<names.Length;i++) {
+            if (sprites[i]!=null) {
+                itemMaterial[i] = new Material(Shader.Find("Sprites/Diffuse"));
+                itemMaterial[i].mainTexture = sprites[i];
+
+                /*
+                itemMaterial[i].SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                itemMaterial[i].SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+                itemMaterial[i].SetInt("_ZWrite", 1);
+                itemMaterial[i].EnableKeyword("_ALPHATEST_ON");
+                itemMaterial[i].DisableKeyword("_ALPHABLEND_ON");
+                itemMaterial[i].DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                itemMaterial[i].renderQueue = 2450;
+                */
             }
         }
     }
@@ -39,5 +60,74 @@ public class Item
         return null;
     }
 
+    public static Mesh getItemMesh() {
+        Mesh itemMesh = new Mesh();
 
+        //set vertices
+
+        Vector3[] vertices = new Vector3[4];
+        vertices[0] = new Vector3(-1, -0.5f, 0);
+        vertices[1] = new Vector3(1, -0.5f, 0);
+        vertices[2] = new Vector3(-1, 0.5f, 0);
+        vertices[3] = new Vector3(1, 0.5f, 0);
+        itemMesh.vertices = vertices;
+
+        //set triangles
+
+        int[] tri = new int[6];
+        //  Lower left triangle.
+        tri[0] = 0;
+        tri[1] = 2;
+        tri[2] = 1;
+        //  Upper right triangle.   
+        tri[3] = 2;
+        tri[4] = 3;
+        tri[5] = 1;
+        itemMesh.triangles = tri;
+
+        //set normals
+
+        Vector3[] normals = new Vector3[4];
+
+        normals[0] = -Vector3.forward;
+        normals[1] = -Vector3.forward;
+        normals[2] = -Vector3.forward;
+        normals[3] = -Vector3.forward;
+
+        itemMesh.normals = normals;
+
+        //set UVs
+
+        Vector2[] uv = new Vector2[4];
+
+        uv[0] = new Vector2(0, 0);
+        uv[1] = new Vector2(1, 0);
+        uv[2] = new Vector2(0, 1);
+        uv[3] = new Vector2(1, 1);
+
+        itemMesh.uv = uv;
+
+        return itemMesh;
+    }
+
+    public static GameObject createItem(Type itemType,Vector3 position) {
+        GameObject item = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        item.AddComponent<ItemBehavior>();
+        item.GetComponent<MeshFilter>().mesh = getItemMesh();
+
+        item.GetComponent<MeshRenderer>().material = itemMaterial[(int) itemType];
+        
+
+        //item.GetComponent<MeshCollider>().sharedMesh = getItemMesh();
+        item.GetComponent<SphereCollider>().isTrigger = true;
+
+
+
+        item.GetComponent<ItemBehavior>().drop();
+
+        item.transform.position = position;
+        return item;
+    }
+
+    
 }
