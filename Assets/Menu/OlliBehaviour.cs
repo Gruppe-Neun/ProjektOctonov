30a17b9b079eefb6d43e1afd8c6c55ace98f8ddb
Gruppe-Neun <leon_buettinghaus@web.de>
2019-06-25 10:43:29 +0200

    Added Main Menu scene

---
+++ Assets/Menu/OlliBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OlliBehaviour : MonoBehaviour
+{
+    public Button startGame;
+    public Button tutorial;
+    public Button options;
+    public Button credits;
+    
+    void Start()
+    {
+        gameObject.transform.position = new Vector3(638, 346, 0);
+    }
+
+    void Update()
+    {
+    }
+
+    public void MoveToStartGame() {
+        gameObject.transform.position = new Vector3(638, 346, 0);
+    }
+
+    public void MoveToTutorial() {
+        gameObject.transform.position = new Vector3(638, 269, 0);
+    }
+
+    public void MoveToOptions() {
+        gameObject.transform.position = new Vector3(638, 192, 0);
+    }
+
+    public void MoveToCredits() {
+        gameObject.transform.position = new Vector3(638, 121, 0);
+    }
+}
