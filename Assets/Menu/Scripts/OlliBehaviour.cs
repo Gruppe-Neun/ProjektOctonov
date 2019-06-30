2b342159cfa12150a03989fb8dee2e244504c35c
Gruppe-Neun <leon_buettinghaus@web.de>
2019-06-30 13:41:26 +0200

    Added new level scenes. Scenes can now be loaded over Menu

--- Assets/Menu/Scripts/OlliBehaviour.cs
+++ Assets/Menu/Scripts/OlliBehaviour.cs
@@ -16,14 +16,14 @@ public class OlliBehaviour : MonoBehaviour
 
     void Update()
     {
-
-
-        if (Time.time > time + 0.5f && buttonPressed) {
+        if (Time.time > time + 0.1f && buttonPressed) {
             gameObject.SetActive(false);
             gameObject.GetComponent<Animator>().SetBool("buttonPressed", false);
             buttonPressed = false;
         }
     }
+
+    //MAIN
 
     public void MoveToStartGame() {
         gameObject.SetActive(true);
@@ -45,16 +45,36 @@ public class OlliBehaviour : MonoBehaviour
         gameObject.transform.position = new Vector3(638, 139, 0);
     }
 
+    //STARTGAME
+
+    public void MoveToLvl1() {
+        gameObject.SetActive(true);
+        gameObject.transform.position = new Vector3(179.8f, 135, 0);
+    }
+
+    public void MoveToLvl2() {
+        gameObject.SetActive(true);
+        gameObject.transform.position = new Vector3(479.1f, 135, 0);
+    }
+
+    public void MoveToLvl3() {
+        gameObject.SetActive(true);
+        gameObject.transform.position = new Vector3(773.4f, 135, 0);
+    }
+
+    //TUTORIAL
+
+    //OPTIONS
+
+    //CREDITS
+
+    //BACK
+
     public void MoveToBack() {
         gameObject.SetActive(true);
         gameObject.transform.position = new Vector3(827, 42, 0);
     }
 
-    public void OnButtonPressed() {
-        gameObject.GetComponent<Animator>().SetBool("buttonPressed", true);
-        time = Time.time;
-        buttonPressed = true;
-    }
 
     public void Deactivate() {
         gameObject.SetActive(false);
---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    public Text progressText;
+    private AsyncOperation operation;
+    private Canvas loadingScreen;
+
+    private void Awake() {
+        loadingScreen = GetComponentInChildren<Canvas>(true);
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadScene(string sceneName) {
+        UpdateProgressText(0);
+        loadingScreen.gameObject.SetActive(true);
+        StartCoroutine(BeginLoad(sceneName));
+    }
+
+    private IEnumerator BeginLoad(string sceneName) {
+        operation = SceneManager.LoadSceneAsync(sceneName);
+        while (!operation.isDone) {
+            UpdateProgressText(operation.progress);
+            yield return null;
+        }
+        UpdateProgressText(operation.progress);
+        operation = null;
+        loadingScreen.gameObject.SetActive(false);
+    }
+
+    private void UpdateProgressText(float progress) {
+        progressText.text = (int)(progress * 100f) + "%";
+    }
+}
